76913c4f35dd3d179989cf40e15b87460e80c43a
bhuban <yadav.bhuban.by@gmail.com>
2021-05-24 11:39:12 +0545

    client side verification completed for login page

--- assests/includes/ajaxReceivedData.php
+++ assests/includes/ajaxReceivedData.php
@@ -3,13 +3,50 @@
 extract($_POST);
 $ret_arr=array();
 
+// for Login
+if (isset($_POST['loginUsername']) && isset($_POST['loginPassword'])) {
+	if (!empty($_POST['loginUsername']) && !empty($_POST['loginPassword'])) {
+		echo json_encode(array('success'=>1));
+	}else{
+	echo json_encode(array('success'=>0));
+	}
+}
+
+// for Account Verification
+if (isset($_POST['accountVerificationOtp'])) {
+	if (!empty($_POST['accountVerificationOtp'])) {
+		echo json_encode(array('success'=>0));
+	}else{
+	echo json_encode(array('success'=>0));
+	}
+}
+
+
+// // for service provider Registration
+if (isset($_POST['serviceProviderFullName'])) {
+	if (!empty($_POST['serviceProviderFullName'])) {
+		echo json_encode(array('success'=>1));
+	}else{
+	echo json_encode(array('success'=>0));
+	}
+}
+
+// // for work request Registration
+if (isset($_POST['workRequestFullName'])) {
+	if (!empty($_POST['workRequestFullName'])) {
+		echo json_encode(array('success'=>1));
+	}else{
+	echo json_encode(array('success'=>0));
+	}
+}
+
 // For reset email
 if (isset($_POST['resetEmail'])){
 	if (!empty($_POST['resetEmail'])) {
 		echo json_encode(array('success'=>1));
 	}else{
 	echo json_encode(array('success'=>0));
-}
+	}
 }
 
 // For otp
